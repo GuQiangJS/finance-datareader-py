b8c8d7c98488725eb48694f3380b830752124be6
GuQiangJS <GuQiangJS@hotmail.com>
2018-07-02 10:08:38 +0800

    version

--- finance_datareader_py/__init__.py
+++ finance_datareader_py/__init__.py
@@ -1,4 +1,9 @@
 # Copyright (C) 2018 GuQiangJs.
 # Licensed under https://www.gnu.org/licenses/gpl-3.0.html <see LICENSE file>
 
+from ._version import get_versions
+
 __all__ = ['netease', 'sohu', 'sse', 'szse']
+
+__version__ = get_versions()['version']
+del get_versions
