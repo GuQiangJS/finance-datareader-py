239ab739dffb3d876bdbc9a722a50f1386e21f2d
GuQiangJS <GuQiangJS@hotmail.com>
2018-08-03 10:12:51 +0800

    增加 从 中证指数有限公司 获取指数的成分权重

--- finance_datareader_py/csindex/__init__.py
+++ finance_datareader_py/csindex/__init__.py
@@ -1,11 +1,12 @@
 # Copyright (C) 2018 GuQiangJs.
 # Licensed under Apache License 2.0 <see LICENSE file>
 
+import pandas as pd
 from pandas import read_excel
 
 
 def get_stock_holdings(index: str):
-    """ 获取指数的成分列表
+    """ 从 中证指数有限公司 获取指数的成分列表
 
     Args:
         index: 指数代码
@@ -41,4 +42,46 @@ def get_stock_holdings(index: str):
     df.rename(columns={'成分券代码Constituent Code': 'symbol',
                        '成分券名称Constituent Name': 'name'}, inplace=True)
     df.set_index("symbol", inplace=True)
+    return df
+
+
+def get_stock_holdings_weight(index: str):
+    """ 从 中证指数有限公司 获取指数的成分权重
+
+        Args:
+            index: 指数代码
+
+        Returns:
+            ``pandas.DataFrame``:
+
+        Examples:
+            .. testcode:: python
+
+                from finance_datareader_py.csindex import get_stock_holdings_weight
+
+                df = get_stock_holdings('000300')
+
+                print(df)
+
+            .. testoutput::
+
+                symbol  name      权重(%)Weight(%)
+                600000  浦发银行            1.11
+                600008  首创股份            0.10
+                600009  上海机场            0.54
+                600010  包钢股份            0.21
+                ...      ...
+        """
+    if not index:
+        raise ValueError()
+
+    url = 'http://www.csindex.com.cn/uploads/file/autofile/' \
+          'closeweight/{0}closeweight.xls'.format(index)
+
+    df = read_excel(url, convert_float=False, dtype=object, usecols=[4, 5, 8])
+    df.rename(columns={'成分券代码Constituent Code': 'symbol',
+                       '成分券名称Constituent Name': 'name'}, inplace=True)
+    df.set_index("symbol", inplace=True)
+    df['权重(%)Weight(%)'] = pd.to_numeric(df['权重(%)Weight(%)']
+                                         , downcast='float')
     return df
--- finance_datareader_py/tests/csindex/test_init.py
+++ finance_datareader_py/tests/csindex/test_init.py
@@ -2,7 +2,9 @@
 # Licensed under Apache License 2.0 <see LICENSE file>
 
 import unittest
+
 from finance_datareader_py.csindex import get_stock_holdings
+from finance_datareader_py.csindex import get_stock_holdings_weight
 
 
 class csindex_TestCase(unittest.TestCase):
@@ -15,6 +17,18 @@ class csindex_TestCase(unittest.TestCase):
     def test_get_stock_holdings_ValueError(self):
         self.assertRaises(ValueError, get_stock_holdings, '')
         self.assertRaises(ValueError, get_stock_holdings, None)
+
+    def test_get_stock_holdings_weight(self):
+        df = get_stock_holdings_weight('000300')
+        self.assertFalse(df.empty)
+        self.assertEqual(300, len(df.index))
+        print(df)
+        self.assertEqual(df.dtypes[0], 'object')
+        self.assertEqual(df.dtypes[1], 'float32')
+
+    def test_get_stock_holdings_weight_ValueError(self):
+        self.assertRaises(ValueError, get_stock_holdings_weight, '')
+        self.assertRaises(ValueError, get_stock_holdings_weight, None)
 
 
 if __name__ == '__main__':
