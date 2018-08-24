dee43816dc3b71a528464f9e782eedec6581b04c
Travis CI User <travis@example.org>
2018-08-24 15:02:12 +0000

    Update docs after building Travis build 121 of GuQiangJS/finance-datareader-py
    The docs were built from the branch 'master' against the commit
    341d11b7983c8dc024f77642057d05672714e755.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/GuQiangJS/finance-datareader-py/jobs/420153538.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy devel --build-tags

--- devel/_sources/readers/index.rst.txt
+++ devel/_sources/readers/index.rst.txt
@@ -1,6 +1,12 @@
 Data Readers
 ------------
 
+.. py:module:: finance_datareader_py
+
+.. autoclass:: DailyReader
+   :members:
+   :undoc-members: url
+
 .. toctree::
    :maxdepth: 3
 
--- devel/_static/documentation_options.js
+++ devel/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.9+9.gc23f145',
+    VERSION: '0.1.9+13.g341d11b',
     LANGUAGE: 'zh_CN',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
