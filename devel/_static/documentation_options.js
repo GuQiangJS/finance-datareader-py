4f9d575a882843f27a48e7a70a92ceb0b618e110
Travis CI User <travis@example.org>
2018-08-31 06:12:32 +0000

    Update docs after building Travis build 131 of GuQiangJS/finance-datareader-py
    The docs were built from the branch 'master' against the commit
    c1bd05386ed932f4bebe52a9b977f69e59ef9054.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/GuQiangJS/finance-datareader-py/jobs/422885451.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy devel --build-tags

--- devel/_static/documentation_options.js
+++ devel/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.5+6.g0a6cc18',
+    VERSION: '0.2.5+8.gc1bd053',
     LANGUAGE: 'zh_CN',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
